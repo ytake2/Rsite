5065165daf218d4b8c329db296a77cbc79dd6e8c
yoshitake TAKEBAYASHI <TAKE2@yoshitake.local>
2016-05-17 18:59:31 +0900

    # lec4 add

--- _source/new-post/new-post.R
+++
@@ -1,3 +0,0 @@
-# This R file accomanies the .Rmd blog post
-# _source/new-post/2016-04-14-new-post.Rmd
-
--- blog/_source/my-first-blog-post/my-first-blog-post.R
+++
@@ -1,3 +0,0 @@
-# This R file accomanies the .Rmd blog post
-# _source/my-first-blog-post/2016-04-14-my-first-blog-post.Rmd
-
